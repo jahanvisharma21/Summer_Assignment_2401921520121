324321db83fe5e8e51a21b94958ccfede1000860
jahanvisharma21 <shjahanvi021@gmail.com>
2026-06-15 10:41:59 +0530

    Week 3 Day 1 Solutions

---
+++ Week_3/Day_1/Middleoflinkedlist.py
@@ -0,0 +1,11 @@
+# Middle of the Linked List
+class Solution:
+    def middleNode(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow        
