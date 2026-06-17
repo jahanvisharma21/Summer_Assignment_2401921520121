75054b182d96834f4fd7ffcf17276ef42332209d
jahanvisharma21 <shjahanvi021@gmail.com>
2026-06-17 10:21:19 +0530

    Week 3 Day 3 Solutions

---
+++ Week_3/Day_3/Validparentheses.py
@@ -0,0 +1,15 @@
+# Valid Parentheses
+class Solution:
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in mapping:
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(char)
+        
+        return len(stack) == 0        
