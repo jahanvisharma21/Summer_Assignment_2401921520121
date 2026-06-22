d2f05fdbd6abdac3852a9ee82b65a04ffa5aea1e
jahanvisharma21 <shjahanvi021@gmail.com>
2026-06-22 08:59:38 +0530

    Week 4 Day 1 Solutions

---
+++ Week_4/Day_1/Invertbinarytree.py
@@ -0,0 +1,13 @@
+# Invert Binary Tree
+class Solution:
+    def invertTree(self, root):
+
+        if root is None:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root       
