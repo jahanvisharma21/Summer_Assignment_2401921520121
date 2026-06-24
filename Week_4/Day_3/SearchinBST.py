bee052d3195cb044a00e7975fbb11d03694b701e
jahanvisharma21 <shjahanvi021@gmail.com>
2026-06-24 09:31:10 +0530

    Week 4 Day 3 Solutions

---
+++ Week_4/Day_3/SearchinBST.py
@@ -0,0 +1,11 @@
+# Search in a Binary Search Tree 
+class Solution:
+    def searchBST(self, root, val):
+        while root:
+            if root.val == val:
+                return root
+            elif val < root.val:
+                root = root.left
+            else:
+                root = root.right
+        return None   
